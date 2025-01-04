0cc38fc94c3514743e1b4ce5d7d544e259bfefe7
Majdi Fahoum <majdif47@gmail.com>
2025-01-04 13:20:06 +0200

    added solution for 405. Convert a Number to Hexadecimal

---
+++ 405. Convert a Number to Hexadecimal.go
@@ -0,0 +1,16 @@
+package main
+
+func toHex(num int) string {
+  if num == 0 {
+    return "0"
+  }
+  n, mask := uint32(num), "0123456789abcdef"
+
+  str := ""
+
+  for n != 0 {
+    str = string(mask [n& 0xF]) + str
+    n >>= 4
+  }
+  return str
+}
