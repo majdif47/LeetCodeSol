6359cbf1ca2f021acab60b90af2aaf009a42f9fa
Majdi Fahoum <majdif47@gmail.com>
2025-01-04 23:29:35 +0200

    added soluttion for 3151. Special Array I

--- 3151. Special Array I.go
+++ 3151. Special Array I.go
@@ -1,13 +1,7 @@
-package main
-import "fmt"
-
-
-
-func isArraySpecial(nums [4]int) bool {
+func isArraySpecial(nums []int) bool {
   for i := 0 ; i <= len(nums) - 2 ; i++ {
     if len(nums) == 1 {return true}
     if nums[i]%2 == nums[i+1] {
-      fmt.Println(nums[i],nums[i+1])
       return false
     }
   }
@@ -15,7 +9,3 @@ func isArraySpecial(nums [4]int) bool {
 }
 
 
-func main() {
-  arr := [4]int{4,3,1,6}
-  isArraySpecial(arr)
-}
